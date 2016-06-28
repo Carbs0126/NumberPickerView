e8688897f93686119f1a52c95f0ec68a885c557c
unknown <Rick.Wang@EBWT009-Rick.e-bao.cn>
2016-06-28 11:21:54 +0800

    first commit

---
+++ app/src/main/java/cn/carbswang/android/numberpickerview/ActivityMain.java
@@ -0,0 +1,119 @@
+package cn.carbswang.android.numberpickerview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import cn.carbswang.android.library.NumberPickerView;
+
+/**
+ * Created by Carbs.Wang.
+ */
+public class ActivityMain extends AppCompatActivity implements View.OnClickListener,
+        NumberPickerView.OnScrollListener, NumberPickerView.OnValueChangeListener {
+
+    private static final String TAG = "picker";
+
+    private NumberPickerView picker;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        picker = (NumberPickerView) this.findViewById(R.id.picker);
+        button1 = (Button) this.findViewById(R.id.button1);
+        button2 = (Button) this.findViewById(R.id.button2);
+        button3 = (Button) this.findViewById(R.id.button3);
+        button4 = (Button) this.findViewById(R.id.button4);
+        button5 = (Button) this.findViewById(R.id.button5);
+        button6 = (Button) this.findViewById(R.id.button6);
+
+        picker.setOnScrollListener(this);
+        picker.setOnValueChangedListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+
+        String[] display_2 = getResources().getStringArray(R.array.test_display_2);
+        picker.refreshByNewDisplayedValues(display_2);
+
+        getWrapState();
+    }
+
+    //获取当前picker是否wrap
+    private void getWrapState() {
+        if (picker.getWrapSelectorWheelAbsolutely()) {
+            button1.setText(R.string.switch_wrap_mode_true);
+        } else {
+            button1.setText(R.string.switch_wrap_mode_false);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                picker.setWrapSelectorWheel(!picker.getWrapSelectorWheelAbsolutely());
+                getWrapState();
+                break;
+            case R.id.button2:
+                String[] display_1 = getResources().getStringArray(R.array.test_display_1);
+                picker.refreshByNewDisplayedValues(display_1);
+                getWrapState();
+                break;
+            case R.id.button3:
+                String[] display_2 = getResources().getStringArray(R.array.test_display_2);
+                picker.refreshByNewDisplayedValues(display_2);
+                getWrapState();
+                break;
+            case R.id.button4:
+                int value = picker.getValue();
+                picker.smoothScrollToValue(value, value + 2);
+                break;
+            case R.id.button5:
+                getCurrentContent();
+                break;
+            case R.id.button6:
+                startActivity(new Intent(ActivityMain.this, ActivityTimePicker.class));
+                break;
+        }
+    }
+
+    private void getCurrentContent(){
+        String[] content = picker.getDisplayedValues();
+        if (content != null)
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.picked_content_is) + content[picker.getValue() - picker.getMinValue()], Toast.LENGTH_SHORT)
+                    .show();
+    }
+
+    @Override
+    public void onScrollStateChange(NumberPickerView view, int scrollState) {
+        Log.d(TAG, "onScrollStateChange : " + scrollState);
+    }
+
+    @Override
+    public void onValueChange(NumberPickerView picker, int oldVal, int newVal) {
+        String[] content = picker.getDisplayedValues();
+        if (content != null)
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.picked_content_is) + content[newVal - picker.getMinValue()], Toast.LENGTH_SHORT)
+                    .show();
+    }
+
+
+}
---
+++ app/src/main/java/cn/carbswang/android/numberpickerview/ActivityTimePicker.java
@@ -0,0 +1,68 @@
+package cn.carbswang.android.numberpickerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import cn.carbswang.android.library.NumberPickerView;
+
+/**
+ * Created by Carbs.Wang on 2016/6/24.
+ */
+public class ActivityTimePicker extends AppCompatActivity implements View.OnClickListener{
+
+    private NumberPickerView mPickerViewH;
+    private NumberPickerView mPickerViewM;
+    private NumberPickerView mPickerViewD;
+    private Button mButtonInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_picker);
+        mPickerViewH = (NumberPickerView)this.findViewById(R.id.picker_hour);
+        mPickerViewM = (NumberPickerView)this.findViewById(R.id.picker_minute);
+        mPickerViewD = (NumberPickerView)this.findViewById(R.id.picker_half_day);
+
+        mButtonInfo = (Button)this.findViewById(R.id.button_get_info);
+        mButtonInfo.setOnClickListener(this);
+
+        initTime();
+    }
+
+    private void initTime(){
+        GregorianCalendar calendar = (GregorianCalendar) GregorianCalendar.getInstance();
+        int h = calendar.get(Calendar.HOUR_OF_DAY);
+        int m = calendar.get(Calendar.MINUTE);
+        int d = h < 12 ? 0 : 1;
+        h = h % 12;
+
+        setData(mPickerViewH, 0, 11, h);
+        setData(mPickerViewM, 0, 59, m);
+        setData(mPickerViewD, 0, 1, d);
+    }
+
+    private void setData(NumberPickerView picker, int minValue, int maxValue, int value){
+        picker.setMinValue(minValue);
+        picker.setMaxValue(maxValue);
+        picker.setValue(value);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_get_info:
+                String h = mPickerViewH.getContentByCurrValue();
+                String m = mPickerViewM.getContentByCurrValue();
+                String d = mPickerViewD.getContentByCurrValue();
+                Toast.makeText(getApplicationContext(),h + getString(R.string.hour_hint) + " "
+                        + m + getString(R.string.minute_hint) + " " + d,Toast.LENGTH_LONG).show();
+            break;
+        }
+    }
+}
